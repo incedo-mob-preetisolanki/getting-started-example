a173d8f27b60d49c85103badc28ae5fbb89e2f40
Amitaibu <amitai@gizra.com>
2015-08-02 12:59:35 +0300

    Hide the navbar on IE, as it's fixed.

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -62,7 +62,10 @@ describe('Visual monitor testing', function() {
       .webdrivercss(testName + '.homepage', {
         name: '1',
         exclude: [],
-        remove: [],
+        remove: [
+          // Hide the navbar on IE, as it's fixed.
+          selectedCaps == 'ie11' ? 'navbar-fixed-top' : '',
+        ],
         screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
       }, shoovWebdrivercss.processResults)
       .call(done);
